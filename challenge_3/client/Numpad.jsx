90e35abe3b728eb1e3b39d8f7d0b94669d26d8b6
Suejung Shin <suejungshin@Suejungs-MBP.attlocal.net>
2020-01-20 21:39:13 -0800

    Build bowling scoreboard and user entry

--- challenge_3/client/Numpad.jsx
+++ challenge_3/client/Numpad.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
-const Numpad = (props) => {
+class Numpad extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="numpad-container">
-      {Array.from({length: 11}, (v, i) => <div className="num-button">{i}</div>)}
-    </div>
-  )
+    this.state = {
+      numPinsHit: 0,
+    }
+
+    this.selectNumPins = this.selectNumPins.bind(this);
+  }
+
+  selectNumPins(event) {
+    let numPins = event.target.id;
+    this.props.hitPins(numPins);
+  }
+
+  render() {
+    return (
+      <div className="numpad-container">
+        {Array.from({ length: 11 }, (v, i) => <div className="num-button" key={i} id={i} onClick={this.selectNumPins}>{i}</div>)}
+      </div>
+    )
+  }
 
 }
 
--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -7,16 +7,45 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-
+      totalScore: 0,
+      frameScore: 0,
+      frameNum: 1,
+      numPinsStanding: 10,
     }
 
+    this.hitPins = this.hitPins.bind(this);
+  }
+
+  hitPins(countOfPins) {
+    let newNumPins = this.state.numPinsStanding - countOfPins;
+    if (newNumPins <= 0) {
+      this.gameOver();
+    } else {
+      this.setState({
+        numPinsStanding: newNumPins,
+        frameNum: this.state.frameNum + 1,
+        frameScore: 10,
+        totalScore: this.state.totalScore + this.state.frameScore
+      }, () => { console.log(this.state) })
+    }
+  }
+
+  updateScore() {
+
+  }
+
+  gameOver() {
+    console.log('Game over!');
   }
 
   render() {
 
     return (
       <div>
-        <Numpad></Numpad>
+        <div>Number of Pins Standing: {this.state.numPinsStanding}</div>
+        <div>Total Score: {this.state.totalScore}</div>
+        <div>Round Number: {this.state.frameNum}</div>
+        <Numpad hitPins={this.hitPins}></Numpad>
       </div>
     )
   }
